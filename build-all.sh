f5cc321685438f658b9138eaed2bfb2c0ea9ae00
Dirk Jagdmann <doj@cubic.org>
2020-06-12 19:41:51 -0700

    build default version last

--- build-all.sh
+++ build-all.sh
@@ -2,11 +2,13 @@
 # build all color schemes of dracopy
 
 set -e
-make clean
-make zip
 
 for i in BLUE SX 128 ; do
     make clean
     CFLAGS=-DCOLOR_SCHEME_$i make zip
     mv dracopy-1.0doj.zip ../doj/c64/dracopy-1.0doj-$i.zip
 done
+
+# build the default version
+make clean
+make zip
